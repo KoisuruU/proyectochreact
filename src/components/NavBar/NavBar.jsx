e44c26a59a33b3dcdbf75926252d6f4dc71ffbcb
Tomas Zazzali <tommyzazzali@gmail.com>
2022-05-03 21:54:18 -0300

    Desafio Menu y Landing - Negocio Kpop + Zazzali

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,39 @@
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Container from 'react-bootstrap/Container'
+import CartWidget from '../CartWidget/CartWidget'
+
+
+const NavBar = () => {
+    return (
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+    <Container>
+    <Navbar.Brand href="#home">Kpop Shop</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+        <Nav.Link href="#features">Girl Groups!</Nav.Link>
+        <Nav.Link href="#pricing">Boy Groups!</Nav.Link>
+        <NavDropdown title="Merchandise" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Light Sticks</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Magazines</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Apparel</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Photocards</NavDropdown.Item>
+        </NavDropdown>
+        </Nav>
+        <Nav>
+            <Nav.Link href="#deets">Support</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+                Login/Signup   
+            </Nav.Link>
+        </Nav>
+    </Navbar.Collapse>
+    <CartWidget/>
+    </Container>
+    </Navbar>
+    )
+}
+
+export default NavBar
